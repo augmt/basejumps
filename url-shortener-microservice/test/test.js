4b8082cc3ddf574e91392bd5e954dc9e29c4acdb
Hector Garcia <soseinhg3@gmail.com>
2016-08-26 20:46:58 +0000

    Add pending tests

---
+++ url-shortener-microservice/test/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('app', function () {
+  describe('when given a URL', function () {
+    it('should respond with json');
+
+    it('should process valid URLs');
+
+    it('should return an error for invalid URLs');
+  });
+
+  describe('when given an alias', function () {
+    it('should redirect to the original link');
+  });
+});
