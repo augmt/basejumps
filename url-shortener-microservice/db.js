b032f0c3584da7f797e7b5efa8de81333e7c27a6
Hector Garcia <soseinhg3@gmail.com>
2016-09-13 21:12:58 +0000

    Disable bufferMaxEntries

--- url-shortener-microservice/db.js
+++ url-shortener-microservice/db.js
@@ -3,6 +3,6 @@
 const mongo = require('mongodb').MongoClient;
 
 module.exports = async function (app) {
-  const db = app.context.db = await mongo.connect(process.env.MONGO_URL);
+  const db = app.context.db = await mongo.connect(process.env.MONGO_URL, {bufferMaxEntries: 0});
   await db.collection('aliases').createIndex({alias: 1, url: 1}, {unique: true});
 };
