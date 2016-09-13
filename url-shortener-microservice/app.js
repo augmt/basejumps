27d019d176480b855917fe69e9855eaa3079f9ee
Hector Garcia <soseinhg3@gmail.com>
2016-09-13 21:00:12 +0000

    Setup error handling

--- url-shortener-microservice/app.js
+++ url-shortener-microservice/app.js
@@ -6,10 +6,21 @@ const cors = require('kcors');
 const json = require('koa-json');
 const router = require('./routes');
 
+const dbPromise = require('./db.js');
+
 const app = module.exports = new Koa();
 
-require('./db.js')(app);
-
-app.use(cors());
-app.use(json());
-app.use(router.routes());
+dbPromise(app).catch(err => app.emit('error', err)).then(() => {
+  app.use(cors());
+  app.use(json());
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      app.emit('error', err);
+      ctx.body = err.message;
+      ctx.status = err.status || 500;
+    }
+  });
+  app.use(router.routes());
+});
